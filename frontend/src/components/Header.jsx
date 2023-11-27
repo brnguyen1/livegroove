028ed65fe99ad09b80207182246974c49985e22f
bnguyen-aa <brandon.nguyen1@aa.com>
2023-11-26 18:08:34 -0600

    finished song display

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
 
     return (
     <div className="fixed top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-        <div className="flex items-center ml-4 mr-auto font-semibold italic">
-            LiveGroove
-        </div>
+        <Link to={"/"}>
+            <div className="flex items-center ml-4 mr-auto font-semibold italic">
+                LiveGroove
+            </div>
+        </Link>
     </div>
     )
 }
--- frontend/src/components/SongDisplay.jsx
+++ frontend/src/components/SongDisplay.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
 import SongBanner from "./SongBanner";
+import { Rating } from "@mui/material";
 
 export default function SongDisplay() {
-    return(
-        <div className="flex flex-col h-5/6 w-2/3 border m-auto">
-            <div className="bg-green-100 w-full h-3/5">
+    const [rating, setRating] = useState(0)
+    
+    function changeRating(new_rating){
+        setRating(new_rating);
+    }
 
+    return(
+        <div className="flex flex-col h-5/6 w-2/3 border my-24 m-auto">
+            <div className="border w-full h-3/5">
+                <div className="bg-green-100 h-4/5 w-full">
+
+                </div>
+                <div className="grid grid-cols-11 h-1/5">
+                    <div className="flex col-start-6 m-auto">
+                        <Rating
+                            value={rating}
+                            onChange={(event, new_rating) => {
+                                changeRating(new_rating);
+                            }}
+                            size="large"
+                        />
+                    </div>
+                    <div className="flex col-start-10 m-auto">
+                        <svg className="h-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                            <path d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416V96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4l192 160L256 241V96c0-17.7 14.3-32 32-32s32 14.3 32 32V416c0 17.7-14.3 32-32 32s-32-14.3-32-32V271l-11.5 9.6-192 160z"/>
+                        </svg>
+                    </div>
+                    
+                </div>
             </div>
             <div className="flex flex-col my-auto gap-y-5">
                 <SongBanner image="word" song_name="Song1"/>
                 <SongBanner image="word" song_name="Song2"/>
                 <SongBanner image="word" song_name="Song3"/>
-
             </div>
         </div>
     )
